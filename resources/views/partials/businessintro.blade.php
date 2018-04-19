ab2442657f0078038d8d3351a2dc926b32ed25b1
Sydney Deale <sydneydeale@gmail.com>
2018-04-19 16:10:39 -0500

    business intro wip

--- resources/views/partials/businessintro.blade.php
+++ resources/views/partials/businessintro.blade.php
@@ -1,3 +1,19 @@
-<div class="business-into h-32 bg-blue-darker">
-
+<div class="bg-cover" style="background-image: url(https://image.shutterstock.com/z/stock-photo-colorful-underwater-scene-of-red-and-yellow-fish-with-blue-water-background-168811532.jpg);">
+    <div class="business-into bg-blue-80i">
+        <div class="pt-8">
+            <img class="block mx-auto sm:h-32 rounded-full mx-auto" src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png" alt="">
+        </div>
+        <div class="flex-auto lg:max-w-lg mx-auto pt-4 pb-8 text-center leading-loose text-white">
+            <h1 class="text-2xl font-medium">Voted Best Charter Captain on the Forgotten Coast, 2015 & 2017!</h1>
+            <p class="text-sm text-grey py-4">Welcome to Phil’s Charter Service. With over 1,000 successful trips on St. Joseph Bay and surrounding waters, I feel
+                confident I can provide you and your family and friends, a safe, enjoyable trip you’ll remember for a lifetime. The
+                variety of fish we catch in St Joe Bay is amazing, including speckled sea trout, red drum, black drum, flounder,
+                sheepshead, Spanish mackerel, pompano, shark and many others. </p>
+            <p class="text-sm text-grey">I provide the experience, equipment, bait and license. All you need to bring is sunscreen, snacks, and drinks. 
+                I’ll even clean your fish, if you want to take home dinner!</p>
+            <div class="pt-4">
+                <button class="text-xs rounded-full px-4 py-2 border border-grey text-grey">MORE ABOUT US</button>
+            </div>
+        </div>    
+    </div>
 </div>
