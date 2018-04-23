2d96794e978158e8242857b718163367dea7a84e
Daron Adkins <daron.adkins@gmail.com>
2018-04-23 12:10:01 -0500

    merge with upstream

--- app/Http/Controllers/ServicesController.php
+++ app/Http/Controllers/ServicesController.php
@@ -7,11 +7,11 @@ use Illuminate\Http\Request;
 
 class ServicesController extends Controller
 {
-
     public function __construct()
     {
         $this->middleware('auth');
     }
+
     /**
      * Display a listing of the resource.
      *
