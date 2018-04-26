b5ed7169816e17c54bf489ba65fd0e6e03ee15eb
programmer-man <bryan@kerigan.com>
2018-04-26 18:20:51 -0400

    content

---
+++ resources/assets/js/components/frontend/GoogleMap.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="google-map" :id="mapName">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    function initMap(){
+        console.log('google maps api loaded.')
+    }
+    export default {
+        props: [
+            'name',
+            'latitude',
+            'longitude',
+            'zoom'
+        ],
+
+        data: function () {
+            return {
+                mapName: this.name + "-map",
+                markers: [],
+                pins: []
+            }
+        },
+
+        // created() {
+        //     let googleMapsApi = document.createElement('script');
+        //     googleMapsApi.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCRXeRhZCIYcKhtc-rfHCejAJsEW9rYtt4&callback=initMap';
+        //     document.getElementsByTagName('head')[0].appendChild(googleMapsApi);
+        // },
+
+        mounted: function () {
+            const element = document.getElementById(this.mapName);
+            const options = {
+                zoom: this.zoom,
+                center: new google.maps.LatLng(this.latitude,this.longitude),
+                disableDefaultUI: true,
+                zoomControl: true,
+                scaleControl: true
+            };
+            const map = new google.maps.Map(element, options);
+            const bounds = new google.maps.LatLngBounds();
+            this.markers = this.$children;
+
+            for(let i = 0; i < this.markers.length; i++){
+                let pin = this.markers[i];
+                this.pins.push({
+                    latitude: pin._data.markerCoordinates.latitude,
+                    longitude: pin._data.markerCoordinates.longitude,
+                });
+
+                const position = new google.maps.LatLng(pin.latitude, pin.longitude);
+                const marker = new google.maps.Marker({
+                    position,
+                    map,
+                    // icon: '/wp-content/themes/kma-slim/img/map-pin.png'
+                });
+
+                const infowindow = new google.maps.InfoWindow({
+                    maxWidth: 279,
+                    content: pin.$refs.infowindow,
+                    title: pin._data.name
+                });
+
+                marker.addListener('click', function(){
+                    infowindow.open(map, marker);
+                });
+
+                bounds.extend(position);
+                map.fitBounds(bounds);
+
+            }
+        },
+
+    }
+</script>
--- resources/views/StaticPages/reviews.blade.php
+++ resources/views/StaticPages/reviews.blade.php
@@ -5,7 +5,7 @@
         <div class="container mx-auto p-4 md:my-10">
             <div class="px-4 pb-4">
                 <h1 class="text-brand-darker mb-3">Reviews</h1>
-                <p class="leading-normal text-grey-darker">Intro text would go here.</p>
+                {{--<p class="leading-normal text-grey-darker">Intro text would go here.</p>--}}
             </div>
             <reviews></reviews>
         </div>
